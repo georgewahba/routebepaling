88955db048369d1823997eef6ef8af1cf486b333
George Wahba <54804560+georgewahba@users.noreply.github.com>
2023-04-25 09:36:01 +0200

    initial commit

---
+++ applicatie_main.java
@@ -0,0 +1,9 @@
+public class applicatie_main {
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                routebepaling_scherm frame = new routebepaling_scherm();
+            }
+        });
+    }
+}
