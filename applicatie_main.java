4d91b898f8cfb379f2f5d3ede860b6490600314f
George Wahba <54804560+georgewahba@users.noreply.github.com>
2023-06-02 15:06:13 +0200

    .

--- applicatie_main.java
+++
@@ -1,9 +0,0 @@
-public class applicatie_main {
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                Routebepaling_scherm frame = new Routebepaling_scherm();
-            }
-        });
-    }
-}
